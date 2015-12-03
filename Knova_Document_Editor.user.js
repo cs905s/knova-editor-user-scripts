4a673c660caa7f7e24e16363ed94ca094419fb25
Divya Mahajan <divya.mahajan@outlook.com>
2015-12-03 08:14:26 -0800

    Corrected user script

--- Knova_Document_Editor.user.js
+++ Knova_Document_Editor.user.js
@@ -1,106 +1,113 @@
 // ==UserScript==
-// @name        Knova Document editor - next doc
+// @name        Knova Document editor
 // @namespace   com.dmahajan.knova
-// @description Enable next document button in editor
+// @description Kanisa Knowledge management add on. In the document editor, add a button to clear out document fields and insert an archiving comment.
 // @include     http://*/KanisaSupportCenter/authoring/editDocument.do*
 // @include     https://*/KanisaSupportCenter/authoring/editDocument*
 // @require     http://code.jquery.com/jquery-2.1.4.js
+// @require https://cdn.firebase.com/js/client/2.3.2/firebase.js
 // @version     1
 // @grant       none
 // ==/UserScript==
-
-
-// Author: Divya Mahajan
-// Revision: 02-Dec-2015
-//
-
-var base_url = 'http://pww.eureka.aai.ms.philips.com/KanisaSupportCenter';
-var doclist_url = 'https://rv52ig.dm2301.livefilestore.com/y3mJYmDC3OHzULYFT6CNyev_7h93nweGXs_kPHa_0EtfRbZkmccnRyoxTvNBtweFwbonR5fjHZ3lQgmme76PLazpIu2OISf6efCE-BXbgQOmFyap_djOgUqZbMgvaleKk2AcfQ1BOnEWEtr6ieYyZjDDQ/knova_list.json?download&psid=1';//http://localhost/~usd05813/knova_list.json';
-var documentIDs = [
-  'No docs'
-];
-//     https://github.com/kimmobrunfeldt/progressbar.js/blob/master/dist/progressbar.min.js
-
-
+var archive_comment = 'This Knova solution has been reviewed and its content has been moved to a validated application for Device master record content. It is no longer available on Knova online.';
 var externalID = $('input[name=documentID]').val();
-var nextDocID = '';
-function JumpToNextDocument()
-{
-  window.alert(nextDocID);
-  window.location = base_url + '/authoring/editDocument.do?externalID=' + nextDocID;
-}
-function LastDocument()
-{
-  window.alert('You are on the last document in the worklist.');
-}
-function setup_button()
-{
-  var disableNextButton = true;
-  var i = documentIDs.indexOf(externalID);
-  var message = '';
-  if (i < 0) {
-    message = 'Editing ' + externalID + '. This is not in the worklist.';
-    document.getElementById('nextDocMessage').innerHTML = message;
-    return;
-  } else if (i == (documentIDs.length - 1))
-  {
-    message = 'Editing ' + externalID + '. Last document';
-    document.getElementById('nextDocMessage').innerHTML = message;
-  } else {
-    i = i + 1;
-    message = ' Editing ' + externalID + ', the next document is ' + documentIDs[i] + '. You are on ' + i + ' out of ' + documentIDs.length + '.';
-    nextDocID = documentIDs[i];
-    document.getElementById('nextDocMessage').innerHTML = message;
-    $('div#myContainer2').append('<button id="myNextDocumentButton" type="button">Next document (' + nextDocID + ')</button>');
-    document.getElementById('myNextDocumentButton').addEventListener('click', JumpToNextDocument, false);
-  }
-  //setup_progress(i, documentIDs.length);
+var workflowID = $('input[name=workflowID').val();
 
-}
-/*
-function setup_progress(current, total) {
-  var circle = new ProgressBar.Circle('#example-percent-container', {
-    color: '#FCB03C',
-    strokeWidth: 3,
-    trailWidth: 1,
-    duration: 1500,
-    text: {
-      value: '0'
-    },
-    step: function (state, bar) {
-      bar.setText((bar.value() * 100).toFixed(0));
-    }
-  });
-  circle.animate((current * 1) / (total * 1), function () {
-    circle.setText(current + '/' + total);
-  });
-}
-*/
-function LoadDocumentIDs() {
-  var section = $('<div id="myContainer2" ><div id="nextDocMessage" class="body"></div></div>');
-  $('span.header3').append(section);
-  //unsafeWindow.documentIDs=['27372','27163','12123'];
-  if (unsafeWindow.documentIDs) {
-    documentIDs = unsafeWindow.documentIDs;
-    console.log(documentIDs.length + ' document IDs loaded from cache ');
-    setup_button();
-  } else {
-    console.log('Loading IDs');
-    $.ajax({
-      dataType: 'jsonp',
-      jsonpCallback: 'data_callback',
-      url: doclist_url
-    }).done(function (data, textStatus, jqXHR) {
-      documentIDs = data;
-      unsafeWindow.documentIDs = data;
-      console.log(documentIDs.length + ' document IDs loaded. ');
-      setup_button();
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-      window.alert('Could not load document IDs - ' + textStatus);
-      document.getElementById('nextDocMessage').innerHTML = 'Next document not available. Could not load document IDs - ' + textStatus;
-    });
-  }
+
+function SetNoteFields(zEvent) {
+  console.log('Set note fields for externalID:'+externalID+ ' workflowID:'+workflowID);
+  var noteFrame = document.getElementById('frmEdtNotes').contentWindow.document;
+  $('textarea', noteFrame).val(archive_comment);
+  $('form', noteFrame).submit();
   
 }
-LoadDocumentIDs();
-console.log("Loading");
+function ArchiveDocument(zEvent) {
+  SetNoteFields(zEvent);
+  setTimeout(SetMainDocFields,1000);  
+}
+function SetMainDocFields(zEvent) {
+  /*--- For our dummy action, we'll just add a line of text to the top
+        of the screen.
+    */
+  console.log('Set main doc fields for externalID:'+externalID+ ' workflowID:'+workflowID);
+  
+  var old_title = $('input[name=title]').val();
+  var new_title = 'ARCHIVE - ' + old_title;
+  if (old_title.substring(0,7)=='ARCHIVE') 
+  {
+    // Already archived - so skip adding prefix to title;
+  } else {
+    $('input[name=title]').val(new_title);
+  }
+  document.getElementById('idContentobj0').contentWindow.document.body.innerHTML=archive_comment;
+  document.getElementById('idContentobj1').contentWindow.document.body.innerHTML=archive_comment;
+  document.getElementById('idContentobj2').contentWindow.document.body.innerHTML="<br>";
+  document.getElementById('idContentobj3').contentWindow.document.body.innerHTML="<br>";
+  document.getElementById('idContentobj4').contentWindow.document.body.innerHTML="<br>";
+  document.getElementById('idContentobj7').contentWindow.document.body.innerHTML="<br>";
+  document.getElementById('idContentobj8').contentWindow.document.body.innerHTML="<br>";
+  /*
+  This way caused errors forcing the changes to be discarded.
+  $('input[name=act]').val('save');
+  $('input[name=XMLProblem_Cause]').val('&nbsp;');
+  $('input[name=XMLProblem_Description_Details]').val(archive_comment);
+  $('input[name=XMLProblem_Solution]').val('&nbsp;');
+  $('input[name=XMLProduct_Information]').val('&nbsp;');
+  $('input[name=XMLSource]').val('&nbsp;');
+  $('input[name=XMLToolsUsed]').val('&nbsp;');
+  $('input[name=XMLPartsUsed').val('&nbsp;');
+  */
+  //$('form[name=editDocForm]').submit();
+  unsafeWindow.saveDoc(); // This is the method called when you click Save draft.
+  unsafeWindow.previewDoc();
+  console.log('Doc saved');
+  
+}
+// User interface setup
+
+function setup_button()
+{
+  // Create a button on top
+  var buttons = $('<div id="myContainer">' + 
+                  '<button id="myButton1" type="button">Set fields for archiving</button>' + 
+                  '</div>');
+  $('span.header3').append(buttons);
+  //--- Activate the newly added button.
+  document.getElementById('myButton1').addEventListener('click', ArchiveDocument, false);
+  console.log('New buttons inserted into the document');
+}
+
+setup_button();
+
+// Rest of this is not really needed but it is cool so I left it here.
+function multilineStr(dummyFunc) {
+  var str = dummyFunc.toString();
+  str = str.replace(/^[^\/]+\/\*!?/, '') // Strip function () { /*!
+  .replace(/\s*\*\/\s*\}\s*$/, '') // Strip */ }
+  .replace(/\/\/.+$/gm, '') // Double-slash comments wreck CSS. Strip them.
+  ;
+  return str;
+}
+
+GM_addStyle(multilineStr(function () { /*!
+    #myContainer {
+        position:               absolute;
+        top:                    0;
+        left:                   0;
+        font-size:              20px;
+        background:             orange;
+        border:                 3px outset black;
+        margin:                 5px;
+        opacity:                0.9;
+        z-index:                222;
+        padding:                5px 20px;
+    }
+    #myButton {
+        cursor:                 pointer;
+    }
+    #myContainer p {
+        color:                  red;
+        background:             white;
+    }
+  */
+}));
